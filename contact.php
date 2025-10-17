cfc67755ac1f0a69be0aacd3545338cfc77d2acc
jasonkid0 <jason.carolino.casas@gmail.com>
2025-10-17 12:34:37 +0800

    first commit

---
+++ contact.php
@@ -0,0 +1,91 @@
+<?php include 'assets/includes/header.php' ?>
+<title>About Us | Saint Francis of Assisi College Bacoor</title>
+</head>
+
+<body>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0" nonce="sSX03XeH"></script>
+
+<?php
+ include ('assets/includes/navbar.php')
+ ?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>Contact Us</h2>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div data-aos="fade-up">
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15456.08611646644!2d120.9698856!3d14.4259225!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d23e0529938f%3A0xcd8900b5875739d!2sSaint%20Francis%20of%20Assisi%20College%20-%20Bacoor%20Campus!5e0!3m2!1sen!2sph!4v1677478237793!5m2!1sen!2sph" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="container" data-aos="fade-up">
+
+        <div class="row mt-5">
+          <div class="col-lg-3 mt-5 mr-5">
+            <div class="fb-page" data-href="https://www.facebook.com/mysfacbacoor/" data-tabs="messages" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/mysfacbacoor/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/mysfacbacoor/">Saint Francis of Assisi College - Bacoor Campus</a></blockquote></div>
+            <!-- <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form> -->
+
+          </div>
+          <div class="col-lg-1 mt-5 ml-5">
+              
+          </div >
+          <div class="col-lg-8 mt-5 ml-5">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>#96 Bayanan, City of Bacoor, Cavite</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>admissions.sfacbacoor@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <h4><b>Basic Education Admission:</b> 0969-080-0657 | (046) 501-1135</h4>
+                <h4><b>College Admission:</b> 0994-706-3287</h4>
+                
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+<?php include('assets/includes/footer.php') ?>
